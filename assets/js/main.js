974b92cc5607713ebd6262b21df92f808de91e44
rado_rakoto <40176014+Tisseurdevie@users.noreply.github.com>
2024-08-07 16:39:38 +0200

    video 3d

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,17 +67,17 @@ document.addEventListener("DOMContentLoaded", function() {
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
-      setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
-  }
+  // const preloader = document.querySelector('#preloader');
+  // if (preloader) {
+  //   window.addEventListener('load', () => {
+  //     setTimeout(() => {
+  //       preloader.classList.add('loaded');
+  //     }, 1000);
+  //     setTimeout(() => {
+  //       preloader.remove();
+  //     }, 2000);
+  //   });
+  // }
 
   /**
    * Scroll top button
